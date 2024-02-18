ec13f9556656f195e4235202951830865ebad738
Brett Pasula <brettpasula@gmail.com>
2024-02-17 20:25:26 -0800

    add tabs to about, upgrade dependencies

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { NgOptimizedImage } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [MatGridListModule, MatCardModule, NgOptimizedImage],
+  imports: [MatGridListModule, MatCardModule, NgOptimizedImage, MatTabsModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
-export class AboutComponent {}
+export class AboutComponent { }
