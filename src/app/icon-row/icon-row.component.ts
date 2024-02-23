fb083b505a7f8bb8f296ce6c74f28f22e3f41af5
Brett Pasula <brettpasula@gmail.com>
2024-02-22 22:43:09 -0800

    update favicon, home, add icon-row, consistent styling

---
+++ src/app/icon-row/icon-row.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'icon-row',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './icon-row.component.html',
+  styleUrl: './icon-row.component.css'
+})
+export class IconRowComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { IconRowComponent } from '../../icon-row/icon-row.component';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [],
+  imports: [IconRowComponent, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
