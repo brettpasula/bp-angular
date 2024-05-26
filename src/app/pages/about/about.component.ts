e10ab09f4ae03e774bc5ddb9a32bcb7355eaa63a
Brett Pasula <brettpasula@gmail.com>
2024-05-25 20:05:01 -0700

    move icon-row to about, remove from home

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -3,13 +3,21 @@ import { MatCardModule } from '@angular/material/card';
 import { GalleryComponent } from '../../components/gallery/gallery.component';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider'; 
+import { MatDividerModule } from '@angular/material/divider';
+import { IconRowComponent } from '../../components/icon-row/icon-row.component';
 
 @Component({
   selector: 'about',
   standalone: true,
-  imports: [MatCardModule, GalleryComponent, MatListModule, MatIconModule, MatDividerModule],
+  imports: [
+    MatCardModule,
+    GalleryComponent,
+    MatListModule,
+    MatIconModule,
+    MatDividerModule,
+    IconRowComponent,
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
-export class AboutComponent { }
+export class AboutComponent {}
