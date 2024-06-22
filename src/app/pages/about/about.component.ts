9b48c406d0f8799934e057d8da9cc8cf9d8ce096
Brett Pasula <brettpasula@gmail.com>
2024-06-21 20:43:30 -0700

    add expansion-panel with extra content to about

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { GalleryComponent } from '../../components/gallery/gallery.component';
-import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { IconRowComponent } from '../../components/icon-row/icon-row.component';
+import { MatExpansionModule, MatExpansionPanel } from '@angular/material/expansion';
 
 @Component({
   selector: 'about',
@@ -12,12 +12,13 @@ import { IconRowComponent } from '../../components/icon-row/icon-row.component';
   imports: [
     MatCardModule,
     GalleryComponent,
-    MatListModule,
     MatIconModule,
     MatDividerModule,
     IconRowComponent,
+    MatExpansionModule,
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
+  viewProviders: [MatExpansionPanel]
 })
 export class AboutComponent {}
